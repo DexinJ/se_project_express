7091e4b32bcd49a4f0371db3ea6e2badc2937b43
DexinJ <50345919+DexinJ@users.noreply.github.com>
2023-10-18 11:59:34 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { PORT = 3001 } = process.env;
+const app = express();
+mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+
+const routes = require("./routes");
+
+app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: "65301929e095495a809833f3",
+  };
+  next();
+});
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
