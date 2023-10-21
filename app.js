b05495109641f43fc88a608c3a15dffbebdb2524
DexinJ <jindexin6@gmail.com>
2023-10-21 09:00:36 -0700

    added helmet

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const helmet = require("helmet");
+const routes = require("./routes");
+const { createUser, login } = require("./controllers/users");
 
 const { PORT = 3001 } = process.env;
 const app = express();
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
-const routes = require("./routes");
-const { createUser, login } = require("./controllers/users");
+app.disable("x-powered-by");
 
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 
 app.post("/signup", createUser);
